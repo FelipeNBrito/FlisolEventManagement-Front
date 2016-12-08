1b00089b1b6b7791f1f9206b2f4d78705c29862c
AllefLobo <allef.lobo@gmail.com>
2016-12-08 01:24:21 -0300

    tradução dos campos

--- views/translate/languages.js
+++ views/translate/languages.js
@@ -1,12 +1,63 @@
   var translations_pt_br = {
-  
+		HOME:	"Início",
+		SETTINGS:	"Configurações",
+		LOGOUT:	"Sair",
+  	ADMINISTRATORS: "Administradores",
+		ADD_ADMINISTRATOR:	"Adicionar Administrador",
+		DELETE_SELECTED:	"Excluir selecionado",
+		SELECT_ALL:	"Selecionar todos",
+		FIRST_NAME:	"Primeiro nome",
+		LAST_NAME:	"Sobrenome",
+		LOGIN: "Entrar",
+		EMAIL:	"Email",
+		PASSWORD:	"Senha",
+		ERROR_LOGIN:	"Erro ao entrar",
+		ARE_YOU_SURE:	"Você tem certeza?",
+		CONFIRM_DELETE_PERSON:	"Confirmar excluir pessoa",
+		YES:	"Sim",
+		NO:	"Não",
+		NO_PEOPLE:	"Não há pessoas"
 };
 
 var translations_en_us = {
-  
+	HOME:	"Home",
+	SETTINGS:	"Settings",
+	LOGOUT:	"Logout",
+	ADMINISTRATORS: "Administrators",
+	ADD_ADMINISTRATOR:	"Add administrator",
+	DELETE_SELECTED:	"Delete selected",
+	SELECT_ALL: "Select all",
+	FIRST_NAME: "First name",
+	LAST_NAME: "Last name",
+	LOGIN:	"Login",
+	EMAIL:	"Email",
+	PASSWORD:	"Password",
+	ERROR_LOGIN:	"Login error",
+	ARE_YOU_SURE:	"Are you sure?",
+	CONFIRM_DELETE_PERSON:	"Confrim delete person",
+	YES:	"Yes",
+	NO:	"No",
+	NO_PEOPLE:	"No people"
 };
 
 
 var translations_es_es = {
-  
+	HOME:	"Inicio",
+	SETTINGS:	"Ajustes",
+	LOGOUT:	"Cerrar sesión",
+	ADMINISTRATORS:	"Administradores",
+	ADD_ADMINISTRATOR:	"Agregar administrador",
+	DELETE_SELECTED:	"Eliminar seleccionado",
+	SELECT_ALL:	"Seleccionar todo",
+	FIRST_NAME:	"Primer nombre",
+	LAST_NAME:	"Apellido",
+	LOGIN:	"Iniciar sesión",
+	EMAIL:	"Email",
+	PASSWORD:	"Contraseña",
+	ERROR_LOGIN:	"Error de inicio de sesión",
+	ARE_YOU_SURE:	"¿Estás seguro?",
+	CONFIRM_DELETE_PERSON:	"Confirmar eliminar persona",
+	YES:	"Sí",
+	NO:	"No",
+	NO_PEOPLE:	"Sin gente"
 };
